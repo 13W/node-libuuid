9aea84786b76d51dbb9d856bb11c0078307e3a1a
Vladimir Bulyga <xx@ccxx.cc>
2016-02-24 18:38:49 +0300

    updated nan to version 2.2.0
    Signed-off-by: Vladimir Bulyga <xx@ccxx.cc>

--- src/uuid.cc
+++ src/uuid.cc
@@ -5,6 +5,7 @@
 
 #undef UUID_STR_LEN
 #define UUID_STR_LEN 36
+
 static inline void my_uuid_unparse_lower(uuid_t in, char *out) {
   int i;
   uuid_unparse(in, out);
@@ -14,21 +15,19 @@ static inline void my_uuid_unparse_lower(uuid_t in, char *out) {
 using namespace v8;
 using namespace node;
 
-NAN_METHOD(Create) {
-  NanScope();
-
+void Create(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   uuid_t id;
   char uuid_str[37];
 
   uuid_generate(id);
   my_uuid_unparse_lower(id, uuid_str);
 
-  NanReturnValue(NanNew<String>(uuid_str));
+  info.GetReturnValue().Set(Nan::New(uuid_str).ToLocalChecked());
 }
 
 
 void init(Handle<Object> exports) {
-  exports->Set(NanNew<String>("create"),
-    NanNew<FunctionTemplate>(Create)->GetFunction());
+  exports->Set(Nan::New("create").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(Create)->GetFunction());
 }
 NODE_MODULE(uuid, init)
